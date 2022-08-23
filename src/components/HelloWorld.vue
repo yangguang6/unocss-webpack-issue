6b9936fd2f6ce558502a26197175207c41122c84
杨光 <yangguang6@corp.netease.com>
2022-08-23 11:08:09 +0800

    feat: init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
+    <h1 class="mb-30px">{{ msg }}</h1>
+    <p class="p-32px">
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+const UnoCSS = require('unocss/webpack').default
+// const { presetUno } = require('unocss/preset-uno')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  configureWebpack: {
+    plugins: [
+      UnoCSS({ /* options */ }),
+    ]
+  }
 })
